8c2a1a0b3a3f85a97496bbcc25901761b19160b0
Jestin Stoffel <jestin.stoffel@gmail.com>
2013-10-26 11:20:45 -0500

    Initial commit

---
+++ git-diff-wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# diff is called by git with 7 parameters:
+# path old-file old-hex old-mode new-file new-hex new-mode
+
+cat "$5" | perl -pi -e 's/  /  \n/g;' > "$5".temp
+diff -u "$2" "$5".temp
+rm "$5".temp
+
+exit
